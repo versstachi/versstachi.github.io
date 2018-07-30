191ac1d86fd52e19c8c84494913a8c9c5079c1f9
versstachi <30925306+versstachi@users.noreply.github.com>
2018-07-30 16:06:59 +0300

    Переверстка Главной
    Переверстка главной страницы. Изменение футера на всех страницах. Правка верхней серой полоски над хедером

--- medini/js/common.js
+++ medini/js/common.js
@@ -98,7 +98,7 @@ $('.slick-popup-nav').slick({
   ]
 }); 
   $(".center").slick({
-    dots: false,
+    dots: true,
     arrows: true,
     infinite: true, 
     slidesToShow: 1,
@@ -109,6 +109,7 @@ $('.slick-popup-nav').slick({
       {
         breakpoint: 768,
         settings: {
+           dots: false,
           slidesToShow: 1,
           slidesToScroll: 1,
          centerMode: false,
@@ -119,16 +120,55 @@ $('.slick-popup-nav').slick({
       // instead of a settings object
     ]
   }); 
-  $('.fade-collection').slick({
+  $('.latest-list').slick({
   dots: false,
-  arrows: false,
+  arrows: true,
   infinite: true,
-  speed: 500,
-  fade: true,
-  cssEase: 'linear',
-  autoplay: true,
-  autoplaySpeed: 5000,
-});
+  speed: 500, 
+  cssEase: 'linear', 
+  slidesToShow: 7,
+  slidesToScroll: 1,
+  prevArrow: '<span class="nav-prev icon-chevron-left"><img src="img/back.svg"  alt=""></span>',
+  nextArrow: '<span class="nav-next icon-chevron-right"><img src="img/next.svg"  alt=""></span>',
+  responsive: [ 
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 6,
+          slidesToScroll: 1,
+         centerMode: false,
+        }
+        },
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 5,
+          slidesToScroll: 1,
+         centerMode: false,
+        }
+        },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+         centerMode: false,
+        }
+        },
+      {
+        breakpoint: 500,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+         centerMode: false,
+        }
+        } 
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+}); 
+  $(".catalog .latest-list").slick('unslick');
 }); 
 
 // Table Responsive Footable
