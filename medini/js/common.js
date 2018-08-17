c513b4091d2e61faecce5867d0d215871b8cfbb9
versstachi <30925306+versstachi@users.noreply.github.com>
2018-08-17 13:15:43 +0300

    Добавлен слайдер на странице каталога
    Добавлен слайдер на странице каталога
    Правка отображения  кнопки купить в один клик

--- medini/js/common.js
+++ medini/js/common.js
@@ -233,7 +233,51 @@ nextArrow: slickInduvidual.prev().find('.nav-prev'),
 prevArrow: slickInduvidual.prev().find('.nav-next')
 });
 }) 
-$(".catalog .latest-list").slick('unslick');
+$(".latest-list-cat").slick({
+  dots: false,
+  arrows: true,
+  infinite: true,
+  speed: 500, 
+  cssEase: 'linear', 
+  slidesToShow: 5,
+  slidesToScroll: 1,
+  prevArrow: '<span class="nav-prev icon-chevron-left"><img src="img/back.svg"  alt=""></span>',
+  nextArrow: '<span class="nav-next icon-chevron-right"><img src="img/next.svg"  alt=""></span>',
+  responsive: [ 
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 5,
+          slidesToScroll: 1,
+         centerMode: false,
+        }
+        },
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 1,
+         centerMode: false,
+        }
+        },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+         centerMode: false,
+        }
+        },
+      {
+        breakpoint: 500,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+         centerMode: false,
+        }
+        } 
+    ]
+});  
 }); 
 // favorite add class
 $(".link-like").on("click", function () {
