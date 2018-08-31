aa4df26ceecea2c37e8c91f4e833c4b992e2187d
versstachi <30925306+versstachi@users.noreply.github.com>
2018-08-31 11:17:50 +0300

    Добавлена аякс подгрузка изображений на странице каталога у карточки товара
    На страницах каталога, в карточку товара , в слайдер добавлена фоновая загрузка изображений.
    Уменьшены в размере картинки стрелочек для листания этого слайдера.
    Подправлено отображение кнопок "купить в один клик" , на странице товара.

--- medini/js/common.js
+++ medini/js/common.js
@@ -242,6 +242,7 @@ $('.slick-popup-nav').slick({
 $('.product-item-pict-caruousel').each(function(){
 var slickInduvidual = $(this);
 slickInduvidual.slick({
+lazyLoad: 'ondemand',
 nextArrow: slickInduvidual.prev().find('.nav-prev'),
 prevArrow: slickInduvidual.prev().find('.nav-next'), 
 });
@@ -257,38 +258,51 @@ $(".latest-list-cat").slick({
   prevArrow: '<span class="nav-prev icon-chevron-left"><img src="img/back.svg"  alt=""></span>',
   nextArrow: '<span class="nav-next icon-chevron-right"><img src="img/next.svg"  alt=""></span>',
   responsive: [ 
-      {
-        breakpoint: 1200,
-        settings: {
-          slidesToShow: 5,
-          slidesToScroll: 1,
-         centerMode: false,
-        }
-        },
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 4,
-          slidesToScroll: 1,
-         centerMode: false,
-        }
-        },
-      {
-        breakpoint: 768,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 1,
-         centerMode: false,
-        }
-        },
-      {
-        breakpoint: 500,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-         centerMode: false,
-        }
-        } 
+    {
+      breakpoint: 1700,
+      settings: {
+        slidesToShow: 6,
+        slidesToScroll: 1,
+        infinite: true
+      }
+    },
+    {
+      breakpoint: 1500,
+      settings: {
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        infinite: true
+      }
+    },
+    {
+      breakpoint: 1200,
+      settings: {
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        infinite: true
+      }
+    },
+    {
+      breakpoint: 991,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
     ]
 });  
 }); 
