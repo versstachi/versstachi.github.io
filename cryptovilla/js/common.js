c50da6c891c290c9af27aff4d05b80d2fa4bfa1d
versstachi <versstachi@gmail.com>
2018-11-22 16:18:45 +0200

    update

--- cryptovilla/js/common.js
+++ cryptovilla/js/common.js
@@ -139,3 +139,10 @@ $( ".menu-btn" ).on( "click", function() {
 	};
 	
 } )();
+$(function(){
+$('a[href^="#"]').click(function(){
+var target = $(this).attr('href');
+$('html, body').animate({scrollTop: $(target).offset().top}, 800);//800 - длительность скроллинга в мс
+return false; 
+}); 
+});
