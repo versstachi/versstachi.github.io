07b040ad5aa6b597ccc49d2a0ad8e15bc7c61699
versstachi <versstachi@gmail.com>
2019-03-11 12:37:33 +0200

    add domytax

---
+++ domytax/js/common.js
@@ -0,0 +1,52 @@
+$(function() { 
+$('.slider-exemple').slick({
+  dots: false,
+  infinite: false,
+  arrows: true,
+  speed: 300,
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  prevArrow: '<div class="slider-left-arrow"> <span class="glyphicon glyphicon-chevron-left"></span></div>',
+  nextArrow: '<div class="slider-right-arrow"> <span class="glyphicon glyphicon-chevron-right"></span></div>',
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: false
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
+
+if ($(window).width() < 1200) {
+  $(".menu").appendTo(".menu-mobile-row");
+$('.menu-hum').click(function(){
+  $('.menu ').toggle("show");
+});
+} 
+else { 
+$('.menu-hum').click(function(){
+  $('.menu').toggle("show");
+});
+}
+});
