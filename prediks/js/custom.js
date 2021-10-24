320331eb9b91c0660070071b19d0817518810812
versstachi <versstachi@gmail.com>
2021-10-24 11:41:49 +0300

    prediks up

--- prediks/js/custom.js
+++ prediks/js/custom.js
@@ -2,36 +2,60 @@
 document.addEventListener("DOMContentLoaded", ready);
 
 function ready() {
-   // includeHTML
-  function includeHTML() {
-    var z, i, elmnt, file, xhttp;
-    /* Loop through a collection of all HTML elements: */
-    z = document.getElementsByTagName("*");
-    for (i = 0; i < z.length; i++) {
-      elmnt = z[i];
-      /*search for elements with a certain atrribute:*/
-      file = elmnt.getAttribute("w3-include-html");
-      if (file) {
-        /* Make an HTTP request using the attribute value as the file name: */
-        xhttp = new XMLHttpRequest();
-        xhttp.onreadystatechange = function() {
-          if (this.readyState == 4) {
-            if (this.status == 200) {elmnt.innerHTML = this.responseText;}
-            if (this.status == 404) {elmnt.innerHTML = "Page not found.";}
-            /* Remove the attribute, and call this function once more: */
-            elmnt.removeAttribute("w3-include-html");
-            includeHTML();
-          }
-        }
-        xhttp.open("GET", file, true);
-        xhttp.send();
-        /* Exit the function: */
-        return;
-      }
-    }
+
+  //  // includeHTML
+  // function includeHTML() {
+  //   var z, i, elmnt, file, xhttp;
+  //   /* Loop through a collection of all HTML elements: */
+  //   z = document.getElementsByTagName("*");
+  //   for (i = 0; i < z.length; i++) {
+  //     elmnt = z[i];
+  //     /*search for elements with a certain atrribute:*/
+  //     file = elmnt.getAttribute("w3-include-html");
+  //     if (file) {
+  //       /* Make an HTTP request using the attribute value as the file name: */
+  //       xhttp = new XMLHttpRequest();
+  //       xhttp.onreadystatechange = function() {
+  //         if (this.readyState == 4) {
+  //           if (this.status == 200) {elmnt.innerHTML = this.responseText;}
+  //           if (this.status == 404) {elmnt.innerHTML = "Page not found.";}
+  //           /* Remove the attribute, and call this function once more: */
+  //           elmnt.removeAttribute("w3-include-html");
+  //           includeHTML();
+  //         }
+  //       }
+  //       xhttp.open("GET", file, true);
+  //       xhttp.send();
+  //       /* Exit the function: */
+  //       return;
+  //     }
+  //   }
+  // }
+  // includeHTML();
+  // // includeHTML
+   
+class HTMLInclude extends HTMLElement {
+  constructor() {
+    super();
+    this.innerHTML = "Loading...";
+    this.loadContent();
   }
-  includeHTML();
-  // includeHTML
+
+  async loadContent() {
+    const source = this.getAttribute("src");
+    if (!source) {
+      throw new Error("No src attribute given.");
+    }
+    const response = await fetch(source);
+    if (response.status !== 200) {
+      throw new Error(`Could not load resource: ${source}`);
+    }
+    const content = await response.text();
+    this.innerHTML = content;
+  }
+}
+
+window.customElements.define("html-include", HTMLInclude);
    
   // scrollContainer
   const scrollContainer = document.querySelector(".assets .dashboard_content_total_info");
